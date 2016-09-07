64c518f2869d78f5243af4cd2d0fb40abc5a1dfb
Neem Serra <neem.serra@gmail.com>
2016-09-06 22:27:21 -0500

    13. Add methods to model to give back an array of letters

--- Boggle/BoggleModel.swift
+++ Boggle/BoggleModel.swift
@@ -9,4 +9,18 @@
 import Foundation
 
 class BoggleModel {
+    func buttonTextArray(numberOfButtons: Int) -> Array<String> {
+        var array = [String]()
+        for i in 0..<numberOfButtons {
+            array.append(randomLetter())
+        }
+        return array
+    }
+    
+    func randomLetter() -> String {
+        let letters : String = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        let rand = Int(arc4random_uniform(26))
+        let stringArray = Array(letters.characters)
+        return String(stringArray[rand])
+    }
 }
